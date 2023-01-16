9c740e4819c8e19ea668837ed145d7a5bf36c601
Victor Catana <victor.cristian12@yahoo.com>
2023-01-16 20:11:19 +0200

    changes

--- DataMining/cameraEdging.py
+++ DataMining/cameraEdging.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy
-
 camera = cv2.VideoCapture(0)
 
 while True:
@@ -14,13 +13,21 @@ while True:
     cv2.imshow('Blurred', img_blur)
 
     # laplacian image filter
-    laplacian = cv2.Laplacian(frame, cv2.CV_64F)
+    laplacian = cv2.Laplacian(src=frame, ddepth=cv2.CV_64F)
     laplacian = numpy.uint8(laplacian)
     cv2.imshow('Laplacian', laplacian)
 
-    # canny edge detection
-    edges = cv2.Canny(img_blur, 40, 70)
+    # canny edge detector
+    edges = cv2.Canny(image=img_blur, threshold1=40, threshold2=70)
     cv2.imshow('Canny', edges)
+
+    # sobel filter
+    sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5)
+    sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5)
+    sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5)
+    cv2.imshow('Sobel X', sobelx)
+    cv2.imshow('Sobel Y', sobely)
+    cv2.imshow('Sobel X Y', sobelxy)
 
     if cv2.waitKey(5) == ord('x'):
         break
